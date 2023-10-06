eaf7995b29adf4fc35361d42897136a8a246aa77
Mohit Chhaya <mohitchhaya24@gmail.com>
2023-10-06 03:57:45 -0500

    hw3

---
+++ q1/callCC/environment/lambda-kompiled/configVars.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+declaredConfigVar_PGM='Exp'
+declaredConfigVars=(
+    'PGM'
+)
---
+++ q2/imp-kompiled/configVars.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+declaredConfigVar_STDIN='String'
+declaredConfigVar_IO='String'
+declaredConfigVar_PGM='Stmts'
+declaredConfigVar_IO='String'
+declaredConfigVars=(
+    'STDIN'
+    'IO'
+    'PGM'
+    'IO'
+)
